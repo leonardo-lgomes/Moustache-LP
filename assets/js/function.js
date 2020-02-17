1421977a4fff0dcb0a488a9ddd8a0bddcac2ba0c
léo lopes <leoolopesz@users.noreply.github.com>
2020-02-16 21:58:19 -0300

    Landing Page finalizada
    - Código Revisado
    - Responsividade funcionando
    - Jquery para validação do formulário funcionando

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
    $('#formulario-continuar').click(function(){
         let nome = $('#nome').val();
+        let cep = $('#cep').val();
         let email = $('#email').val();
-        let ddd = $('#ddd').val();
-        let telefone = $('#telefone').val(); 
+        let endereco = $('#endereco').val();
+        let numero = $('#numero').val();
+        let telefone = $('#telefone').val();
+        let bairro = $('#bairro').val();
+        let datanascimento = $('#data').val();
+        let cidade = $('#cidade').val();
+        let estado = $('#estado').val();
        
 
        
         if(nome == ""){
-            alert("É necessário informar o nome completo.");
+            alert("É necessário informar o nome.");
             $('#nome').focus();
+            return false;
+        }
+
+        if(cep == ""){
+            alert("É necessário informar o cep.");
+            $('#cep').focus();
             return false;
         }
 
@@ -19,45 +31,37 @@ $(document).ready(function(){
             return false;
         }
 
-        if(ddd == ""){
-            alert("É necessário informar o ddd.");
-            $('#ddd').focus();
+        if(endereco  == ""){
+            alert("É necessário informar o endereço.");
+            $('#endereco').focus();
             return false;
         }
+
+        if(numero  == ""){
+            alert("É necessário informar o número.");
+            $('#numero').focus();
+            return false;
+        }
+
 
         if(telefone == ""){
             alert("É necessário informar o telefone.");
             $('#telefone').focus();
             return false;
         }
-        
-        $('#div-primeiro-passo').addClass('collapse');
-        $('#div-segundo-passo').removeClass('collapse');
-   });
 
-   $('#formulario').submit(function(){
-        let tipo_divida_cartao = $('#tipo_divida_cartao');
-        let tipo_divida_cheque_especial = $('#tipo_divida_cheque_especial');
-        let tipo_divida_emprestimo_especial = $('#tipo_divida_emprestimo_especial');
-        let tipo_divida_outros = $('#tipo_divida_outros');
-        let divida = $('#divida').val();
-        let mensagem = $('#mensagem').val();
-
-        if(!tipo_divida_cartao.is(":checked") && !tipo_divida_cheque_especial.is(":checked") && !tipo_divida_emprestimo_especial.is(":checked") && !tipo_divida_outros.is(":checked")){
-            alert("É necessário informar ao menos um tipo de dívida.");
+        if(bairro  == ""){
+            alert("É necessário informar o bairro.");
+            $('#bairro').focus();
             return false;
         }
 
-        if(divida == ""){
-            alert("É necessário informar o valor da dívida.");
-            $('#divida').focus();
+        if(data  == ""){
+            alert("É necessário informar a data de nascimento.");
+            $('#data').focus();
             return false;
         }
         
-        if(mensagem == ""){
-            alert("É necessário informar a mensagem.");
-            $('#mensagem').focus();
-            return false;
-        }
+       
    });
-});
+}); 
